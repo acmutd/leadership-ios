d43cccfdd4a069702d9c1435e3b55c6b3714c9a4
Harsha Srikara <harshasrikara@gmail.com>
2022-06-15 18:14:57 -0500

    Initial Commit

---
+++ leadership/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  leadership
+//
+//  Created by Harsha Srikara on 6/15/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ leadership/leadershipApp.swift
@@ -0,0 +1,17 @@
+//
+//  leadershipApp.swift
+//  leadership
+//
+//  Created by Harsha Srikara on 6/15/22.
+//
+
+import SwiftUI
+
+@main
+struct leadershipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
